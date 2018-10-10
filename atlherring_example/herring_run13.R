9a5a354be536fe99afdd97af6fde9ada06b93fc9
perretti <ctperretti@gmail.com>
2018-10-10 16:48:42 -0400

    Update atlherring_example/herring_run13.R

--- atlherring_example/herring_run13.R
+++ atlherring_example/herring_run13.R
@@ -1,4 +1,5 @@
 library(stockassessment)
+library(dplyr)
 
 setwd("/Users/charlesperretti/Projects/state-space-tests/atlherring_example/")
 
@@ -54,6 +55,73 @@ pdf(file = "parameter_plot.pdf")
 parplot(fit)
 dev.off()
 
-# Simulate from the fitted model and re-fit
+
+#### Simulate from the fitted model and re-fit ####
 simdat <- simulate(fit, seed = 1, nsim = 1)[[1]]
+
+simfit <- sam.fit(simdat, conf, par)
+
+ssbplot(fit)
+ssbplot(simfit, add = TRUE, col = "blue")
+
+fbarplot(fit, partial = FALSE)
+fbarplot(simfit, partial = FALSE, add = TRUE, col = "blue")
+
+recplot(fit)
+recplot(simfit, add = TRUE, col = "blue")
+
+catchplot(fit)
+catchplot(simfit, add = TRUE, col = "blue")
+
+
+# Run lots of simulations and fits
+simlist <- simulate(fit, seed=1, nsim=10)
+library(parallel)
+no_cores <- detectCores() - 1 #how many cores can we use
+if( no_cores>2 ) no_cores <- 2 # Cran check does not allow us to use more than two 
+cl <- makeCluster(no_cores) #set up some number of nodes
+
+clusterExport(cl, c("conf", "par")) #send these objects to each node
+clusterEvalQ(cl, {library(stockassessment)}) #load the package to each node
+simfitslist <- parLapply(cl, simlist, function(x){sam.fit(x, conf, par)}) #do sam.fit to element
+stopCluster(cl) #shut it down
+
+ssbplot(fit)#the original data
+trash <- lapply(simfitslist, function(x){ssbplot(x, ci=FALSE, add=TRUE)})
+
+fbarplot(fit, partial = FALSE)
+trash <- lapply(simfitslist, function(x){fbarplot(x, ci=FALSE, partial = FALSE, add=TRUE)})
+
+recplot(fit)
+trash <- lapply(simfitslist, function(x){recplot(x, ci=FALSE, add=TRUE)})
+
+catchplot(fit)
+trash <- lapply(simfitslist, function(x){catchplot(x, ci=FALSE, add=TRUE)})
+
+# Extract parameter estimates from the true model
+# remove estiamtes of missing values since they are not 
+# in the simulated dataset and we want the two to match up
+df_true <- 
+  summary(fit$sdrep) %>%
+  as.data.frame() %>%
+  dplyr::mutate(variable = row.names(.)) %>%
+  dplyr::filter(!grepl('missing', variable))
+
+# Extract parameter estimates from the first replicate of the
+# simulated data
+df_replicate1 <-
+  summary(simfitslist[[1]]$sdrep) %>%
+  as.data.frame() %>%
+  dplyr::mutate(variable = row.names(.))
+
+# Make sure the variables match
+all(df_true$variable == df_replicate1$variable)
+
+
+# Calculate proportion of true parameters that fall within the 
+# 95% interval of the estimates
+mean(df_true$Estimate < df_replicate1$Estimate + 1.96*df_replicate1$`Std. Error` &
+     df_true$Estimate > df_replicate1$Estimate - 1.96*df_replicate1$`Std. Error`)
+
+
 
