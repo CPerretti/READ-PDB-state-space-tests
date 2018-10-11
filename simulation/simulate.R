4a45c06e03474a85ede7319e7988fcd355c534a6
perretti <ctperretti@gmail.com>
2018-10-11 15:55:44 -0400

    Update simulation/simulate.R

--- simulation/simulate.R
+++ simulation/simulate.R
@@ -1,5 +1,17 @@
-# See if we can replicate the simulate feature for a fitted model
-n_ages <- 
+# See if we can replicate the simulate feature for a fitted model 
+# (start simple, not running yet)
+
+# Use herring data and model fit to set up simulation
+
+
+# Run simulation
+n_a <- 8 # number of age-classes
+n_t <- 20 # length of time series
+
 for i in 2:n_t {
-  N[1, i] <- w[1, i-1] * p[, i-1] %*% N[, i-1] * exp(rnorm(1))  
+  N[1, i] <- r_func(w[, i-1] * p[, i-1] %*% N[, i-1]) * exp(rnorm(1))
+  N[-c(1, n_a), i] <- N[-c(n_a-1, n_a), i-1] *
+                      exp(f[-c(n_a-1, n_a), i-1]) * 
+                      exp(m[-c(n_a-1, n_a), i-1]) * exp(rnorm(1))
+  N[n_a, i] <- 
 }
