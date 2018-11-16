8c46bb8d31952b433f050ccdab3384b775d118d3
cmlegault <chris.legault@noaa.gov>
2018-11-16 10:37:42 -0500

    add comparison of dervied and SAM proc err sd

---
+++ simulation/misc_scripts/whySAMsdsolarge.R
@@ -0,0 +1,41 @@
+# trying to determine what is causing process errors to be so large for herring
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(stockassessment)
+
+# Use atl herring fit to set up simulation
+load("../atlherring_example/output/fitHer.Rdata")
+
+# get process errors
+f <- exp(fitHer$pl$logF[(fitHer$conf$keyLogFsta[1,] + 1),])
+m <- t(fitHer$data$natMor)
+z <- f + m
+sdN <- exp(fitHer$pl$logSdLogN[(fitHer$conf$keyVarLogN + 1)])
+nA <- ncol(fitHer$data$propF) # number of age-classes
+nT <- fitHer$data$noYears # length of time series
+errPro <- matrix(data = NA,
+                 nrow = nA, 
+                 ncol = nT-1)
+errPro[1, ] <- exp(fitHer$pl$logN[1, 2:nT]) / exp(fitHer$pl$logN[1, 1:(nT-1)])
+errPro[-c(1, nA), ] <-  exp(fitHer$pl$logN[-c(1, nA), 2:nT]) / 
+  (exp(fitHer$pl$logN[-c(nA-1, nA), 1:(nT-1)]) *
+     exp(-z[-c(nA-1, nA), 1:(nT-1)]))
+errPro[nA, ] <- exp(fitHer$pl$logN[nA, 2:nT]) / 
+  (exp(fitHer$pl$logN[nA-1, 1:(nT-1)]) *
+     exp(-z[nA-1, 1:(nT-1)]) +
+     exp(fitHer$pl$logN[nA, 1:(nT-1)]) *
+     exp(-z[nA, 1:(nT-1)]))
+
+errPro
+
+# compute standard deviations of process errors at age and compare to SAM estimates
+derivedsd <-apply(errPro, 1, sd)
+sddf <- data.frame(age = 1:nA,
+                   derivedsd = derivedsd,
+                   SAMsd = sdN)
+sddf
+# why are SAM estimates larger than actual variability among ages?
+
+
